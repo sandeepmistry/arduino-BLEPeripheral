3b60be9a497ff39d9849b5e90e5a7667013b9c26
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-01-19 20:46:55 -0500

    - in memory bond store for non-AVR

--- BLEBondStore.cpp
+++ BLEBondStore.cpp
@@ -4,34 +4,59 @@
   #warning "BLEBondStore persistent storage not supported on this platform"
 #endif
 
+#include "Arduino.h"
+
 #include "BLEBondStore.h"
 
 BLEBondStore::BLEBondStore(int offset) :
+#ifndef __AVR__
+    _dataLength(0),
+    _data(NULL),
+#endif
   _offset(offset)
 {
+}
+
+BLEBondStore::~BLEBondStore() {
+#ifndef __AVR__
+  this->clearData();
+#endif
 }
 
 bool BLEBondStore::hasData() {
 #ifdef __AVR__
   return (eeprom_read_byte((unsigned char *)this->_offset) != 0x00);
 #else
-  return false;
+  return (this->_dataLength > 0);
 #endif
 }
 
 void BLEBondStore::clearData() {
 #ifdef __AVR__
   eeprom_write_byte((unsigned char *)this->_offset, 0x00);
+#else
+  this->_dataLength = 0;
+  if (this->_data) {
+    free(this->_data);
+
+    this->_data = NULL;
+  }
 #endif
 }
 
-void BLEBondStore::storeData(unsigned char* data, unsigned char length) {
+void BLEBondStore::storeData(const unsigned char* data, unsigned char length) {
 #ifdef __AVR__
   eeprom_write_byte((unsigned char *)this->_offset, length);
 
   for (unsigned char i = 0; i < length; i++) {
     eeprom_write_byte((unsigned char *)this->_offset + i + 1, data[i]);
   }
+#else
+  this->_data = (unsigned char *)malloc(length);
+
+  memcpy(this->_data, data, length);
+
+  this->_dataLength = length;
 #endif
 }
 
@@ -40,5 +65,7 @@ void BLEBondStore::restoreData(unsigned char* data, unsigned char length) {
   for (unsigned char i = 0; i < length; i++) {
     data[i] = eeprom_read_byte((unsigned char *)this->_offset + i + 1);
   }
+#else
+  memcpy(data, this->_data, length);
 #endif
 }
--- BLEBondStore.h
+++ BLEBondStore.h
@@ -5,14 +5,19 @@ class BLEBondStore
 {
   public:
     BLEBondStore(int offset = 0);
+    ~BLEBondStore();
 
     bool hasData();
     void clearData();
-    void storeData(unsigned char* data, unsigned char length);
+    void storeData(const unsigned char* data, unsigned char length);
     void restoreData(unsigned char* data, unsigned char length);
 
   private:
-    int _offset;
+    int             _offset;
+#ifndef __AVR__
+    unsigned char   _dataLength;
+    unsigned char*  _data;
+#endif
 };
 
 #endif
