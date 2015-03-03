c9cf724796e1e4c59dfb1625f32e22b40671c5bf
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-03-02 21:42:39 -0500

    - rework bond store to use chunked get/put

--- BLEBondStore.cpp
+++ BLEBondStore.cpp
@@ -19,26 +19,17 @@ BLEBondStore::BLEBondStore(int offset) :
   _offset(offset)
 #elif defined(NRF51) || defined(__RFduino__)
   _flashPageStartAddress((uint32_t *)(NRF_FICR->CODEPAGESIZE * (NRF_FICR->CODESIZE - 1 - (uint32_t)offset)))
-#else
-  _dataLength(0),
-  _data(NULL)
 #endif
 {
 }
 
-BLEBondStore::~BLEBondStore() {
-#if !defined(__AVR__) && !defined(NRF51) && !defined(__RFduino__)
-  this->clearData();
-#endif
-}
-
 bool BLEBondStore::hasData() {
 #ifdef __AVR__
-  return (eeprom_read_byte((unsigned char *)this->_offset) != 0x00);
+  return (eeprom_read_byte((unsigned char *)this->_offset) == 0x01);
 #elif defined(NRF51) || defined(__RFduino__)
   return (*this->_flashPageStartAddress != 0xFFFFFFFF);
 #else
-  return (this->_dataLength > 0);
+  return false;
 #endif
 }
 
@@ -63,24 +54,17 @@ void BLEBondStore::clearData() {
 
   // wait until ready
   FLASH_WAIT_READY
-#else
-  this->_dataLength = 0;
-  if (this->_data) {
-    free(this->_data);
-
-    this->_data = NULL;
-  }
 #endif
 }
 
-void BLEBondStore::storeData(const unsigned char* data, unsigned char length) {
+void BLEBondStore::putData(const unsigned char* data, unsigned char offset, unsigned char length) {
 #ifdef __AVR__
-  eeprom_write_byte((unsigned char *)this->_offset, length);
+  eeprom_write_byte((unsigned char *)this->_offset, 0x01);
 
   for (unsigned char i = 0; i < length; i++) {
-    eeprom_write_byte((unsigned char *)this->_offset + i + 1, data[i]);
+    eeprom_write_byte((unsigned char *)this->_offset + offset + i + 1, data[i]);
   }
-#elif defined(NRF51) || defined(__RFduino__)
+#elif defined(NRF51) || defined(__RFduino__) // ignores offset
   this->clearData();
 
   // turn on flash write enable
@@ -89,9 +73,7 @@ void BLEBondStore::storeData(const unsigned char* data, unsigned char length) {
   // wait until ready
   FLASH_WAIT_READY
 
-  *this->_flashPageStartAddress = length;
-
-  uint32_t *out = (this->_flashPageStartAddress + 1);
+  uint32_t *out = this->_flashPageStartAddress;
   uint32_t *in  = (uint32_t*)data;
 
   for(unsigned char i = 0; i < length; i += 4) { // assumes length is multiple of 4
@@ -109,33 +91,17 @@ void BLEBondStore::storeData(const unsigned char* data, unsigned char length) {
 
   // wait until ready
   FLASH_WAIT_READY
-#else
-  this->_data = (unsigned char *)malloc(length);
-
-  memcpy(this->_data, data, length);
-
-  this->_dataLength = length;
 #endif
 }
 
-unsigned char BLEBondStore::restoreData(unsigned char* data, unsigned char length) {
-  unsigned char storedLength = 0;
-
+void BLEBondStore::getData(unsigned char* data, unsigned char offset, unsigned char length) {
 #ifdef __AVR__
-  storedLength = eeprom_read_byte((unsigned char *)this->_offset);
-
-  length = min(storedLength, length);
-
   for (unsigned char i = 0; i < length; i++) {
-    data[i] = eeprom_read_byte((unsigned char *)this->_offset + i + 1);
+    data[i] = eeprom_read_byte((unsigned char *)this->_offset + offset + i + 1);
   }
-#elif defined(NRF51) || defined(__RFduino__)
-  storedLength = *(this->_flashPageStartAddress);
-
+#elif defined(NRF51) || defined(__RFduino__) // ignores offset
   uint32_t *in = (this->_flashPageStartAddress + 1);
   uint32_t *out  = (uint32_t*)data;
-
-  length = min(storedLength, length);
 
   for(int i = 0; i < length; i += 4) { // assumes length is multiple of 4
     *out = *in;
@@ -143,13 +109,5 @@ unsigned char BLEBondStore::restoreData(unsigned char* data, unsigned char lengt
     out++;
     in++;
   }
-#else
-  storedLength = this->_dataLength;
-
-  length = min(storedLength, length);
-
-  memcpy(data, this->_data, length);
 #endif
-
-  return storedLength;
 }
