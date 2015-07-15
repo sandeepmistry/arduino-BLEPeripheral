808e1f728851a101637880d0a53ddf97d688d491
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-07-15 12:13:56 -0400

    - Eddystone URL beacon example

---
+++ examples/Eddystone/EddystoneURL/EddystoneURL.ino
@@ -0,0 +1,30 @@
+// Import libraries (EddystoneBeacon depends on SPI)
+#include <SPI.h>
+#include <EddystoneBeacon.h>
+
+// define pins (varies per shield/board)
+//
+//   Adafruit Bluefruit LE   10, 2, 9
+//   Blend                    9, 8, UNUSED
+//   Blend Micro              6, 7, 4
+//   RBL BLE Shield           9, 8, UNUSED
+
+#define EDDYSTONE_BEACON_REQ   6
+#define EDDYSTONE_BEACON_RDY   7
+#define EDDYSTONE_BEACON_RST   4
+
+EddystoneBeacon eddystoneBeacon = EddystoneBeacon(EDDYSTONE_BEACON_REQ, EDDYSTONE_BEACON_RDY, EDDYSTONE_BEACON_RST);
+
+void setup() {
+  Serial.begin(9600);
+
+  delay(1000);
+
+  eddystoneBeacon.begin(0x20, "http://www.example.com"); // power, URI
+
+  Serial.println(F("Eddystone URL Beacon"));
+}
+
+void loop() {
+  eddystoneBeacon.loop();
+}
