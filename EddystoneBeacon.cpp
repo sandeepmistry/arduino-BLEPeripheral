ad21fdebc80685890a0b92bb113481104208daad
Arun Kumar <arun271@users.noreply.github.com>
2016-01-27 11:31:25 +0530

    Minor additions to the url prefix/suffix.

--- EddystoneBeacon.cpp
+++ EddystoneBeacon.cpp
@@ -7,8 +7,8 @@
 #define FLAGS_TLM 0x20
 
 static const char* EDDYSTONE_URL_BEACON_PREFIX_SUBSTITUTIONS[] = {
-  "w.",//for http://www.
-  "ws.", //for https://www.
+  "http://www.",//for http://www.
+  "https://www", //for .
   "http://",
   "https://",
   "urn:uuid:",
