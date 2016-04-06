851ce16f2ca52ccd608bc2554e39421306664ac5
Arun Kumar <arun271@users.noreply.github.com>
2016-04-06 22:54:59 +0530

    Add tlm_begin() and tempLoop() for EddystoneTLM.

--- EddystoneBeacon.h
+++ EddystoneBeacon.h
@@ -11,9 +11,9 @@ class EddystoneBeacon : public BLEPeripheral
 
     void begin(char power, const BLEUuid& uid);
     void begin(char power, const char* uri);
-    void begin(const unsigned char* temp, unsigned char len); //For TLM
+    void tlm_begin(); //add //For TLM
     void loop();
-
+    void tempLoop(int analogPin);
     void setURI(const char* uri);
 
   private:
