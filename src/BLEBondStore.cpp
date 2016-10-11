6d0dbb3153a17169c616b23ef603b514fd434d10
Joerg Hubschneider <joerg.hubschneider@bosch-si.com>
2016-10-11 09:22:45 +0200

    contribution remak in source file
    Signed-off-by: Joerg Hubschneider <joerg.hubschneider@bosch-si.com>

--- src/BLEBondStore.cpp
+++ src/BLEBondStore.cpp
@@ -1,5 +1,9 @@
 // Copyright (c) Sandeep Mistry. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// Contributors:
+//    Bosch Software Innovations GmbH - Please refer to git log
+
 
 #ifdef __AVR__
   #include <avr/eeprom.h>
