a97acf56460ddd1dd6b44b1e163022926681d6b7
Joerg Hubschneider <joerg.hubschneider@bosch-si.com>
2016-10-23 22:47:09 +0200

    Bondstore (clear/put) functional for s130 and s132 using the softdevice API (#92)
    * using softdevice functions in blocking way for BondStore clearData() & putData().
    
    Signed-off-by: Joerg Hubschneider <joerg.hubschneider@bosch-si.com>
    
    * clean up BondStore.cpp
    
    Signed-off-by: Joerg Hubschneider <joerg.hubschneider@bosch-si.com>
    
    * contribution remak in source file
    
    Signed-off-by: Joerg Hubschneider <joerg.hubschneider@bosch-si.com>
    
    * remove comments and contributor remark
    
    Signed-off-by: Joerg Hubschneider <joerg.hubschneider@bosch-si.com>
    
    * Reactivate previous direct register handling for BondStore::clearData() and putData() in case of RFdunio.
    
    Signed-off-by: Joerg Hubschneider <joerg.hubschneider@bosch-si.com>

--- src/BLEBondStore.cpp
+++ src/BLEBondStore.cpp
@@ -1,10 +1,16 @@
 // Copyright (c) Sandeep Mistry. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+
 #ifdef __AVR__
   #include <avr/eeprom.h>
-#elif defined(NRF51) || defined(NRF52) || defined(__RFduino__)
-  // nothing extra needed
+#elif defined(__RFduino__)
+  #include <utility/RFduino/nrf_soc.h>
+#define FLASH_WAIT_READY { \
+  while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {}; \
+}
+#elif defined(NRF51) || defined(NRF52)
+  #include <nrf_soc.h>
 #else
   #warning "BLEBondStore persistent storage not supported on this platform"
 #endif
@@ -12,12 +18,6 @@
 #include "Arduino.h"
 
 #include "BLEBondStore.h"
-
-#if defined(NRF51) || defined(NRF52) || defined(__RFduino__)
-#define FLASH_WAIT_READY { \
-  while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {}; \
-}
-#endif
 
 BLEBondStore::BLEBondStore(int offset)
 #if defined(__AVR__) || defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MKL26Z64__)
@@ -41,7 +41,16 @@ bool BLEBondStore::hasData() {
 void BLEBondStore::clearData() {
 #if defined(__AVR__) || defined(__MK20DX128__) || defined(__MK20DX256__) || defined(__MKL26Z64__)
   eeprom_write_byte((unsigned char *)this->_offset, 0x00);
-#elif defined(NRF51) || defined(NRF52) || defined(__RFduino__)
+#elif defined(NRF51) || defined(NRF52)
+
+  int32_t pageNo = (uint32_t)_flashPageStartAddress/NRF_FICR->CODEPAGESIZE;
+  uint32_t err_code;
+  do {
+	err_code = sd_flash_page_erase(pageNo);
+  } while(err_code == NRF_ERROR_BUSY);
+
+#elif defined(__RFduino__)
+
   // turn on flash erase enable
   NRF_NVMC->CONFIG = (NVMC_CONFIG_WEN_Een << NVMC_CONFIG_WEN_Pos);
 
@@ -59,6 +68,7 @@ void BLEBondStore::clearData() {
 
   // wait until ready
   FLASH_WAIT_READY
+
 #endif
 }
 
@@ -69,10 +79,17 @@ void BLEBondStore::putData(const unsigned char* data, unsigned int offset, unsig
   for (unsigned int i = 0; i < length; i++) {
     eeprom_write_byte((unsigned char *)this->_offset + offset + i + 1, data[i]);
   }
-#elif defined(NRF51) || defined(NRF52) || defined(__RFduino__) // ignores offset
+#elif defined(NRF51) || defined(NRF52)  // ignores offset
   this->clearData();
 
-  offset = offset;
+  uint32_t err_code;
+  do {
+	  err_code = sd_flash_write((uint32_t*)_flashPageStartAddress, (uint32_t*)data, (uint32_t)length/4);
+  } while(err_code == NRF_ERROR_BUSY);
+
+#elif defined(__RFduino__) // ignores offset
+
+  this->clearData();
 
   // turn on flash write enable
   NRF_NVMC->CONFIG = (NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos);
@@ -98,6 +115,7 @@ void BLEBondStore::putData(const unsigned char* data, unsigned int offset, unsig
 
   // wait until ready
   FLASH_WAIT_READY
+
 #endif
 }
 
