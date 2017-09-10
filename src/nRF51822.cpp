e6117755e841db643211a1836d16565eacbee4a1
David Sanders <dsanders11@ucsbalum.com>
2017-09-10 06:54:00 -0600

    Fix indication deadlock

--- src/nRF51822.cpp
+++ src/nRF51822.cpp
@@ -1081,15 +1081,9 @@ bool nRF51822::updateCharacteristicValue(BLECharacteristic& characteristic) {
       }
 
       if (localCharacteristicInfo->indicateSubscribed) {
-        if (this->_txBufferCount > 0) {
-          this->_txBufferCount--;
+        hvxParams.type = BLE_GATT_HVX_INDICATION;
 
-          hvxParams.type = BLE_GATT_HVX_INDICATION;
-
-          sd_ble_gatts_hvx(this->_connectionHandle, &hvxParams);
-        } else {
-          success = false;
-        }
+        success = sd_ble_gatts_hvx(this->_connectionHandle, &hvxParams) == NRF_SUCCESS;
       }
     }
   }
@@ -1140,7 +1134,7 @@ bool nRF51822::canNotifyCharacteristic(BLECharacteristic& /*characteristic*/) {
 }
 
 bool nRF51822::canIndicateCharacteristic(BLECharacteristic& /*characteristic*/) {
-  return (this->_txBufferCount > 0);
+  return true;
 }
 
 bool nRF51822::canReadRemoteCharacteristic(BLERemoteCharacteristic& characteristic) {
